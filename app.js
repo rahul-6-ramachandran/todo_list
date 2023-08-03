f644630a54865dbf2b5f74ff8edcec257977cc48
Rahul Ramachandran <rahulramachandran432@gmail.com>
2023-08-03 13:26:16 +0530

    add database

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
-const { log } = require('console')
-const app = express()
-const date = require(__dirname+'/date.js')
+const mongoose = require('mongoose')
+const _ = require('lodash')
 
-const activites = ["Fooding","football","Coding"]
+const app = express()
+
+
 const workItems = []
 
 app.use(bodyParser.urlencoded({extended:true}))
@@ -14,40 +15,144 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine','ejs')
 app.use(express.static('public'))
 
-app.get('/',function(req,res){
-    let day = date.getDate()
-    res.render('list',{
-        listTitle : day,
-        newListItem : activites
-    })
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://0.0.0.0:27017/todolistdb',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
+
+const db = mongoose.connection
+db.once('open', () => console.log('db connected'));
+
+const itemSchema = mongoose.Schema({
+    name : String
+})
+
+const Activity = mongoose.model("Activity",itemSchema)
+
+const Activity1 = new Activity({
+    name:"Breakfast"
+})
+const Activity2 = new Activity({
+    name:"Coding"
+})
+
+const Activity3 = new Activity({
+    name:"Football"
+})
+
+const dataActivites = [Activity1,Activity2,Activity3]
+
+
+const listSchema = {
+    name : String,
+    item : [itemSchema]
+}
+
+const list = mongoose.model('List',listSchema)
+
+
+app.get('/',function(req,res){
+    Activity.find({}).then(function (foundItems) {
+        
+        if(foundItems.length === 0){
+            Activity.insertMany(dataActivites).then(function () {
+                console.log("Successfully saved defult items to DB");
+              }).catch(function (err) {
+                console.log(err);
+              });
+              res.redirect('/')
+        }else{
+            res.render('list',{
+                listTitle : "Today",
+                newListItems : foundItems
+            })
+        }
+        
+       
+      }).catch(function (err) {
+        console.log(err);
+      });
+})
+
+app.get('/:category',function(req,res){
+    
+    var category = _.capitalize(req.params.category) 
+    list.findOne({name:category}).then(function(foundList){
+        if(foundList){
+            res.render('list',{
+                listTitle : foundList.name,
+                newListItems : foundList.item
+            })
+        }else{
+            const List = new list({
+                name : category,
+                items : dataActivites
+            })
+            List.save()
+            res.redirect('/'+ category)
+        }
+    })
+   
+})
+
 app.post('/',function(req,res){
 
-    let activity = req.body.newList
+    const activity = req.body.newList
+    const listName = req.body.list
+    
+   
+    const newActivity = new Activity({
+        name : activity
+    })
 
-    if(req.body.list === "Work List"){
-        workItems.push(activity)
-        res.redirect('/work')   
+    if (listName === "Today"){
+        newActivity.save()
+        res.redirect('/')   
     }else{
-        activites.push(activity)
-        res.redirect('/')
+        list.findOne({name:listName}).then(function(foundList){
+            // console.log(foundList)
+            foundList.item.push(newActivity)
+            foundList.save()
+            res.redirect('/' + listName) 
+        })
     }
-
     
     
 })
 
 // For Work activites
-app.get('/work',function(req,res){
-    res.render('list',{
-        listTitle : "Work List",
-        newListItem : workItems
-    })
-})
+
+
 app.post('/work',(req,res)=>{
     var item = req.body.newList
     workItems.push(item)
     res.redirect('/work')
+})
+
+app.post('/delete',(req,res)=>{
+    const checkedItemId = req.body.checkbox
+    const listName = req.body.listName
+
+    if(listName === "Today"){
+
+        Activity.findByIdAndRemove(checkedItemId).then(function(err){
+            if(err){
+                console.log(err)
+            }else{
+                console.log("Deleted Successfully")
+            }
+            res.redirect('/')
+          
+        })
+
+    }else{
+
+        list.findOneAndUpdate({name:listName},{$pull: {item : {_id: checkedItemId}} }).then(function(foundItems){
+            res.redirect('/' + listName)
+        })
+    }
+    
 })
 
 
